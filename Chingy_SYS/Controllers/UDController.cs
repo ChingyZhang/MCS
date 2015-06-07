b42bb6dc906c6dde2826273026ed1f21b4651f97
Chingy <chingyzhang@outlook.com>
2015-06-07 23:12:31 +0800

    新建项目

---
+++ Chingy_SYS/Controllers/UDController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chingy_SYS.Controllers
+{
+    public class UDController : Controller
+    {
+        //
+        // GET: /UD/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
