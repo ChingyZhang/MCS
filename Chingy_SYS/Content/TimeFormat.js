3702b91ecd2f7cd3ce2dd886bf9e79f18b790c97
ChingyZhang <chingyzhang@outlook.com>
2015-07-30 23:29:04 +0800

    1、时间格式化方法独立到js文件 2、字典列表escape后存储到cookie中，在第二次读取cookie时eval()方法发生错误
    Signed-off-by: ChingyZhang <chingyzhang@outlook.com>

---
+++ Chingy_SYS/Content/TimeFormat.js
@@ -0,0 +1,82 @@
+﻿
+/**
+* 时间对象的格式化
+*/
+Date.prototype.format = function (format) {
+    /*
+    * format="yyyy-MM-dd hh:mm:ss";
+    */
+    var o = {
+        "M+": this.getMonth() + 1,
+        "d+": this.getDate(),
+        "h+": this.getHours(),
+        "m+": this.getMinutes(),
+        "s+": this.getSeconds(),
+        "q+": Math.floor((this.getMonth() + 3) / 3),
+        "S": this.getMilliseconds()
+    }
+
+    if (/(y+)/.test(format)) {
+        format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4
+        - RegExp.$1.length));
+    }
+
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(format)) {
+            format = format.replace(RegExp.$1, RegExp.$1.length == 1
+            ? o[k]
+            : ("00" + o[k]).substr(("" + o[k]).length));
+        }
+    }
+    return format;
+}
+
+var Common = {
+
+    //EasyUI用DataGrid用日期格式化
+    TimeFormatter: function (value, row, index) {
+        if (value == undefined) {
+            return "";
+        }
+        /*json格式时间转js时间格式*/
+        value = value.substr(1, value.length - 2);
+        var obj = eval('(' + "{Date: new " + value + "}" + ')');
+        var dateValue = obj["Date"];
+        if (dateValue.getFullYear() < 1900) {
+            return "";
+        }
+
+        var val = dateValue.format("yyyy-mm-dd HH:MM");
+        return val.substr(11, 5);
+    },
+    DateTimeFormatter: function (value, row, index) {
+        if (value == undefined) {
+            return "";
+        }
+        /*json格式时间转js时间格式*/
+        value = value.substr(1, value.length - 2);
+        var obj = eval('(' + "{Date: new " + value + "}" + ')');
+        var dateValue = obj["Date"];
+        if (dateValue.getFullYear() < 1900) {
+            return "";
+        }
+
+        return dateValue.format("yyyy-mm-dd HH:MM");
+    },
+
+    //EasyUI用DataGrid用日期格式化
+    DateFormatter: function (value, row, index) {
+        if (value == undefined) {
+            return "";
+        }
+        /*json格式时间转js时间格式*/
+        value = value.substr(1, value.length - 2);
+        var obj = eval('(' + "{Date: new " + value + "}" + ')');
+        var dateValue = obj["Date"];
+        if (dateValue.getFullYear() < 1900) {
+            return "";
+        }
+
+        return dateValue.format("yyyy-mm-dd");
+    }
+};
