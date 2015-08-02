63a1c759eac81d73ff6b1282aa23b623e034db59
ChingyZhang <chingyzhang@outlook.com>
2015-08-02 23:05:53 +0800

    备份数据库 尝试在后台进行数据缓存

---
+++ Help/Common/Cache/DataCache.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Cache
+{
+    public class DataCache
+    {
+        public void a()
+        { 
+            //HttpRunTime
+            HttpRuntime.
+        }
+    }
+}
